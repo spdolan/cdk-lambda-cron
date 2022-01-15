280635a666a2bb21381c400fa843a5cd97d3746d
Sean Dolan <sdolan@theclimateservice.com>
2022-01-15 09:53:19 -0700

    Initial commit

---
+++ test/cdk-lambda-cron.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaCron from '../lib/cdk-lambda-cron-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-cron-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaCron.CdkLambdaCronStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
